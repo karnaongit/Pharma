cf89e2a6d1f355eff6f8976ef690781a831fbf4f
Karan Gawande <karangawande08@gmail.com>
2026-01-10 00:10:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-4xl font-bold text-white">Tailwind is working!</h1>
+    </div>
+  )
+}
+
